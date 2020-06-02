e719bf3d5e093b2176de96594cb0d7ffd2ac032d
Adão <adahox@aix.com.br>
2020-06-02 18:04:55 -0300

    better logic & sintax

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,19 +1,21 @@
-Array.prototype.bubbleSort = function() {
-    let enumerable = this.length;
-    let aux = 0;
-    while (enumerable > 1) {
-      for (let x = 0; x < enumerable; x++) {
-        let actual = this[x];
-        let next = this[x + 1];
-        if (actual > next) {
-          [this[x], this[x + 1]] = [this[x + 1], this[x]]
-        }
+Array.prototype.bubbleSort = function () {
+  let enumerable = this.length;
+  let aux = 0;
+  let finished;
+  do {
+    finished = false;
+    for (let x = 0; x < enumerable; x++) {
+      let actual = this[x];
+      let next = this[x + 1];
+      if (actual > next) {
+        [this[x], this[x + 1]] = [this[x + 1], this[x]];
+        finished = true;
       }
-      enumerable--;
     }
-    return this;
-  }
-  
-  let data = [5, 3, 1, 2, 4];
-  console.log(data.bubbleSort());
-  
+    enumerable--;
+  } while (finished);
+  return this;
+};
+
+let data = [5, 3, 1, 2, 4];
+console.log(data.bubbleSort());
